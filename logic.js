4336c281892b25bcf7cd6c64003630310fb50fbb
samar <sasa-2010-1995@hotmail.com>
2017-11-22 14:35:44 +0200

    kls

--- logic.js
+++ logic.js
@@ -9,17 +9,15 @@
     getId(id).addEventListener(eventName, callback);//3callback
   }
 
-  function fetch(url, callback, callbackError) {
+  function fetch(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         var response = JSON.parse(xhr.responseText);
-        callback(response);
+        callback(null,response);
       } else {
         console.log('Status Code: ' + xhr.status);
-        if (callbackError) {
-          callbackError(xhr.status);
-        }
+        callback({err:xhr.status});
       }
     });
     xhr.open('GET', url);
@@ -89,12 +87,22 @@
     title2.innerHTML = character_3.titles.length ? "Title :: " + character_3.titles[0] : "Dont have title";
   }
 
-  fetch("https://api.got.show/api/characters/", function (response) {
-    characters = response;
-  }, function (status) {
-    console.log(status);
+  fetch("https://api.got.show/api/characters/", function (err,response) {
+    if(err) console.log(err);
+    else {
+      fetch("https://www.behindthename.com/api/lookup.php?name=" + character.name.split(" ")[0] +"&key=no007783",
+       function (response) {
+         origins = response;
+         name.innerHTML = origins.
+       });
+     }
+    //  characters = response;
   });
-
+  // fetch("https://www.behindthename.com/api/lookup.php?name=" + character.name.split(" ")[0] +"&key=no007783", function (response) {
+  //   characters = response;
+  // }, function (status) {
+  //   console.log(status);
+  // });
 
   addListener("generator", 'click', function (event) {
     event.preventDefault();
